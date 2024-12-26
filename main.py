afef3b78fb1c3df599505d86ee6acfecff960ad2
AlexPr06 <freezay00828002@gmail.com>
2024-12-26 22:11:21 +0200

    I've added functionality of flipping cards and showing translation.In addition, timer-flip is well-throughout so that it doesn't flip if User will change cards rapidly

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 import pandas
 from tkinter import *
 import random
@@ -16,38 +18,33 @@ list_French_English = list(zip(list_of_french_words, list_of_english_words))
 
 print(list_French_English)
 
-
+current_word = []
 # +----------------------------------          Functions           --------------------------------+
 def word_generation():
     random_pair = random.choice(list_French_English)
     return random_pair
 
 
+# +-------------------------------    Functionality functions   -----------------------------------+
+def next_card():
+    global current_word, flip_timer
+    window.after_cancel(flip_timer)
+    canvas.delete(current_word)
+    current_word = word_generation()
+    new_word = current_word[0]
+    canvas.itemconfig(card_title, text="French", font=("Arial", 40, "italic"), fill="black")
+    canvas.itemconfig(card_word, text=new_word, font=("Arial", 60, "bold"), fill="black")
+    canvas.itemconfig(card_background, image=card_front_image)
+    flip_timer= window.after(3000, func=flip_card)
 
 
-def unknown_button_pressed():
-    global text_id
-    # Видалення попереднього тексту
+def flip_card():
+    global current_word
 
-    canvas.delete(text_id)
-
-    # Вибір випадкового слова зі списку
-    new_word = word_generation()[0]
-
-    # Додавання нового тексту
-    text_id = canvas.create_text(400, 263, text=new_word, font=("Arial", 60, "bold"))
-
-def known_button_pressed():
-    global text_id
-
-    canvas.delete(text_id)
-
-    # Вибір випадкового слова зі списку
-    new_word = word_generation()[0]
-
-    # Додавання нового тексту
-    text_id = canvas.create_text(400, 263, text=new_word, font=("Arial", 60, "bold"))
-
+    translated_word = current_word[1]
+    canvas.itemconfig(card_title, text="English", font=("Arial", 40, "italic"), fill="white")
+    canvas.itemconfig(card_word, text=translated_word, font=("Arial", 60, "bold"), fill="white")
+    canvas.itemconfig(card_background, image=card_back_image)
 
 # +-------------------------------      GUI           -----------------------------------------------+
 
@@ -56,21 +53,26 @@ window = Tk()
 window.title("Flash Card App")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 #creating canvas and use it in window object
 canvas = Canvas(window, width = 800,height = 526)
 
-#extracting image card_back.png
+#extracting image card_front.png
 card_front_image = PhotoImage(file="..\Flash Card App\images\card_front.png")
+
+#extracting image card_back.png
+card_back_image = PhotoImage(file="..\Flash Card App\images\card_back.png")
 
 #using image card_back.png in canvas. x=800/2 = 400, y=256 /2 = 263  ->
 # (400, 256) - center of the canvas and exactly there with push it
-canvas.create_image(400, 263, image=card_front_image)
+card_background =canvas.create_image(400, 263, image=card_front_image)
 
 # Title
-canvas.create_text(400, 150, text= "Title", font=("Arial", 40, "italic"))
+card_title = canvas.create_text(400, 150, text= "", font=("Arial", 40, "italic"))
 
 # Word
-text_id = canvas.create_text(400, 263, text= word_generation()[0], font=("Arial", 60, "bold"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 
 
 #some configurations with canvas object
@@ -82,12 +84,15 @@ canvas.grid(row = 0, column = 0, columnspan = 2)
 
 wrong_image = PhotoImage(file="..\Flash Card App\images\wrong.png")
 
-unknown_button = Button(window, command=unknown_button_pressed, image=wrong_image, highlightthickness=0)
+unknown_button = Button(window, command=next_card, image=wrong_image, highlightthickness=0)
 unknown_button.grid(row = 1, column = 0)
 
 right_image = PhotoImage(file="..\Flash Card App\images\\right.png")
-known_button = Button(window, command=known_button_pressed, image=right_image, highlightthickness=0)
+known_button = Button(window, command=next_card, image=right_image, highlightthickness=0)
 known_button.grid(row = 1, column = 1)
+
+next_card()
+
 
 
 
